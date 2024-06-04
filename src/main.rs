2d71a08926b94928eaf73f57dc7c1b366f81db78
OMGeeky <aalaalgmx@gmail.com>
2024-06-04 20:39:41 +0200

    remove unused stuff

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-#![allow(unused)]
 use lazy_static::lazy_static;
 use twba_common::prelude::*;
 
@@ -14,8 +13,6 @@ lazy_static! {
 #[tokio::main]
 async fn main() -> Result<()> {
     let _guard = init_tracing("twba_uploader");
-    let args = std::env::args().collect::<Vec<_>>();
-    let presentation_mode = args.len() > 1;
     info!("Hello, world!");
 
     run().await?;
@@ -27,20 +24,13 @@ async fn main() -> Result<()> {
 #[tracing::instrument]
 async fn run() -> Result<()> {
     trace!("run");
-    let x = &CONF.google;
-    debug!("{:?}", x);
-    let conf = &CONF.clone();
-    dbg!(conf);
 
     trace!("creating db-connection with db url: {}", &CONF.db_url);
     let db = twba_local_db::open_database(Some(&CONF.db_url)).await?;
     trace!("migrating db");
     twba_local_db::migrate_db(&db).await?;
-    // local_db::print_db(&db).await?;
 
     trace!("creating client");
-    // dbg!(&conf);
-
     let client = client::UploaderClient::new(db).await?;
     trace!("uploading videos");
     client.upload_videos().await?;
